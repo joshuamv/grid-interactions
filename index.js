9cf7dfc059733f26857c2a81b82ab8418803db88
Josh <joshuajmv@me.com>
2020-05-20 18:54:29 +0300

    ok

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
+var code = [];
+
+var simon = [];
+
+var imgSrc = ["img/break1.svg", "img/break2.svg", "img/break3.svg", "img/break4.svg", "img/break5.svg", "img/break6.svg", "img/break7.svg"];
+var counter = 0;
+
+
 $(document).ready(function() {
   //wait for html
 
@@ -11,33 +19,90 @@ $(document).ready(function() {
       $(this).removeClass("color2");
     }
   });
-  
+
+  $("#break-img").on("click", changeSrc);
+
+  // if (simon.length == 4) {
+  //   checkSimon();
+  //   // run checkSimon and reset array
+  // }
+
 });
 
-
-function mini() {
-
+function checkSimon() {
+  if (simon[0] == 2 && simon[1] == 4 && simon[2] == 4 && simon[3] == 2){
+    console.log("okoko");
+    $(".simon-btn").hide();
+    $(".simon-answer").show();
+  }else{
+    simon = [];
+  }
 }
 
+function simonReset(){
+  simon = [];
+  $('#simon-beep')[0].play();
+}
+
+function simonEntered(color) {
+  if ((simon.length) > 2) {
+    simon.push(color);
+    checkSimon();
+    console.log(simon);
+    simon = [];
+    $('#simon-beep')[0].play();
+  }else{
+    simon.push(color);
+    console.log(simon);
+    $('#simon-beep')[0].play();
+  }
+}
+
+function checkCode() {
+  if (code[0] == 2 && code[1] == 3 && code[2] == 3 && code[3] == 5){
+    code = ["UNLOCKED"]
+    $("#screen-text").text(code.join(""));
+    $(".key").prop("onclick", null).off("click");
+    $(".button").addClass(".button-active");
+    $('#ok')[0].play();
+  }else{
+    code = [];
+    code.push("ERROR");
+    $("#screen-text").text(code.join(""));
+    $('#error')[0].play();
+  }
+}
+
+function keyEntered(number) {
+  $("#screen-text").text(code);
+  if (code == "ERROR") {
+    code = [];
+    code.push(number);
+    console.log(code.join(""));
+    $("#screen-text").text(code.join(""));
+    $('#key-beep')[0].play();
+  }else{
+    code.push(number);
+    console.log(code.join(""));
+    $("#screen-text").text(code.join(""));
+    $('#key-beep')[0].play();
+  }
+}
+
+function changeSrc() {
+  if (counter == 6) {
+    return;
+  } else{
+    counter = (counter+1)%imgSrc.length;
+    $("#break-img").attr("src", imgSrc[counter]);
+    $('#crack')[0].play();
+  }
+}
 
 function sky() {
   //sound of birds
   //clouds move
 }
-
-function clap() {
-  //sound
-}
-
-
-function polkaDot() {
-
-}
-
-function alcohol() {
-
-}
-
 
 function glide() {
 
